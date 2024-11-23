472311b12bb0c6acafc3898bb7f2b0e05e3f293f
Vishal Jangid <vishaljangid2@outlook.com>
2024-11-23 17:53:08 +0700

    Popppins font added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./app.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "inter-thin": ["Poppins_100Thin"],
+        "inter-regular": ["Poppins_400Regular"],
+        "inter-semiBold": ["Poppins_600SemiBold"],
+        "inter-bold": ["Poppins_700Bold"],
+      },
+      colors: {
+        primary: "#49D6D8"
+      }
+    },
   },
   plugins: [],
 }
